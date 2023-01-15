7db868d71e40fcc6485c065366ce2b2cf15abbd9
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-01-15 00:05:46 -0500

    🧹 chore: start defining some message types for interfaces

---
+++ microcontroller_application/interfaces/message_types.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Any
+
+# Any is used as a placeholder for all the types we expect
+
+
+# Interface 01.1
+FromPreferencesToProxy = Any
+# Interface 01.2
+FromProxyToPreferences = Any
+
+# Interface 02
+# Interface 03
+# Interface 04
+# Interface 05
+FromEnvironmentToControl = Any
+# Interface 06
+# Interface 07
+FromPreferencesToControl = Any
+# Interface 08
+# Interface 09
+# Interface 10
+# Interface 11
+# Interface 12
+# Interface 13
+# Interface 14
+# Interface 15
