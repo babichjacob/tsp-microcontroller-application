3fe5e5ca618c119adc4e8922036b08bcfbcce46a
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-01-15 00:09:21 -0500

    🧹 chore: annotate interfaces for the preferences module

--- microcontroller_application/modules/m05_preferences/__init__.py
+++ microcontroller_application/modules/m05_preferences/__init__.py
@@ -1,10 +1,16 @@
 from bounded_channel import Receiver, Sender
 
+from microcontroller_application.interfaces.message_types import (
+    FromPreferencesToControl,
+    FromPreferencesToProxy,
+    FromProxyToPreferences,
+)
+
 
 async def run(
     *,
-    from_proxy_module: Receiver,
-    to_proxy_module: Sender,
-    to_control_module: Sender,
+    from_proxy_module: Receiver[FromProxyToPreferences],
+    to_proxy_module: Sender[FromPreferencesToProxy],
+    to_control_module: Sender[FromPreferencesToControl],
 ):
     print("hello from the preferences module")
