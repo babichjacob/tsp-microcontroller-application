3b2f690f6c9cf5ecec57e5319734677aaebeee34
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-02-16 21:30:31 -0500

    implement some message types

--- microcontroller_application/interfaces/message_types.py
+++ microcontroller_application/interfaces/message_types.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import Any
+
+from option_and_result import Option
 
 # Any is used as a placeholder for all the types we expect
 
@@ -7,7 +10,20 @@ from typing import Any
 # Interface 01.1
 FromPreferencesToProxy = Any
 # Interface 01.2
-FromProxyToPreferences = Any
+class UserSlot(Enum):
+    # The system only has to support 4 users (requirement 3.5.5)
+    One = 1
+    Two = 2
+    Three = 3
+    Four = 4
+
+
+@dataclass
+class FromProxyToPreferences:
+    user_slot: UserSlot
+    # The Preferences definition from 4.3.7 (below)
+    preferences: "Preferences"
+
 
 # Interface 02
 FromAggregationToProxy = Any
@@ -16,11 +32,84 @@ FromHumanDetectionToActivityRecognition = Any
 # Interface 04
 FromHumanDetectionToPersonIdentification = Any
 # Interface 05
-FromActivityRecognitionToControl = Any
+class Activity(Enum):
+    # These numbers don’t matter; you just have to give enum variants a value
+    Working = 0
+    Lying = 1
+    Neither = 2
+
+
+@dataclass
+class FromActivityRecognitionToControl:
+    activities_of_humans: list[Activity]
+
+
 # Interface 06
-FromPersonIdentificationToControl = Any
+# An Option has two variants:
+# * None, which doesn’t have a value
+# * Some(x), where x is the value
+# Here, a None value means that a person could not be identified
+# and a Some value contains which trusted user was identified
+IdentifiedPerson = Option[UserSlot]  # Reusing the UserSlot definition in 4.3.1.2
+
+
+@dataclass
+class FromPersonIdentificationToControl:
+    identified_people: list[IdentifiedPerson]
+
+
 # Interface 07
-FromPreferencesToControl = Any
+@dataclass
+class LightEffectDesiredBrightness:
+    """
+    The person wants the brightness in the room to “end up” being this value,
+    taking into account the ambient light already present in it.
+    They are also allowing the system to manipulate the desired brightness
+    depending on the activity they are doing.
+    """
+
+    lumens: float  # 0 (pitch black) to 2000
+    # (a guesstimate for sunlight going through a window into the room)
+    # The system will do its best to bridge the gap between wanting it to be bright
+    # on a dark and cloudy day, for example, when working with just a light bulb
+    # (600 lumens (requirement 3.1.3))
+
+
+@dataclass
+class LightEffectForceEndBrightness:
+    """
+    The person wants to completely override the “smart” functionality of the system
+    and just make the brightness the light bulb emits this exact value.
+    """
+
+    lumens: int  # 0 (for off) to 600 (the maximum per requirement 3.1.3)
+
+
+# A light effect can be either of these two
+LightEffect = LightEffectDesiredBrightness | LightEffectForceEndBrightness
+
+
+@dataclass
+class Timer:
+    effect: LightEffect
+    weekday: int  # 0 to 6
+    hour: int  # 0 to 23
+    minute: int  # 0 to 59
+
+
+@dataclass
+class Preferences:
+    name: str
+    timers: list[Timer]
+
+
+@dataclass
+class FromPreferencesToControl:
+    "The preferences of each person in the room"
+
+    preferences: list[Preferences]  # sorted by weekday, then day, then hour
+
+
 # Interface 08
 FromProxyToPersonIdentification = Any
 # Interface 09
