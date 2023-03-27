1f76fc28b895e18be8d8626524d7a92118110474
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-27 00:55:33 -0400

    log to files as well (will be used for testing)

--- microcontroller_application/log.py
+++ microcontroller_application/log.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from sys import stdout
 
 import logging
+import logging.handlers
 from time import gmtime
 
 
@@ -55,15 +56,48 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record)
 
 
+class CustomFormatterColorless(logging.Formatter):
+    def format(self, record):
+        dt = datetime.fromtimestamp(record.created)
+        dt_utc = dt.astimezone(timezone.utc)
+
+        record.fmttime = dt_utc.strftime("%Y-%m-%d %H:%M:%S.%f %Z")
+
+        shortened_path = Path(record.pathname).relative_to(CWD)
+
+        path_line = f"{shortened_path}:{record.lineno}"
+        record.pathline = path_line
+
+        log_fmt = BASE_FORMAT.format(color_start="", color_end="")
+        formatter = logging.Formatter(log_fmt)
+        formatter.converter = gmtime
+        return formatter.format(record)
+
+
+# https://stackoverflow.com/a/67213458
+def date_namer(default_name: str) -> str:
+    base_filename, ext, date = default_name.split(".")
+    return f"{base_filename}.rolled_over_at_{date}.{ext}"
+
+
+logs_directory = Path("logs")
+logs_directory.mkdir(exist_ok=True)
+log_file = logs_directory / "log.txt"
+root_logger = logging.getLogger()
+file_handler = logging.handlers.TimedRotatingFileHandler(
+    log_file, when="midnight", backupCount=15
+)
+file_handler.namer = date_namer
+file_handler.setFormatter(CustomFormatterColorless())
+root_logger.addHandler(file_handler)
+
+stdout_handler = logging.StreamHandler(stdout)
+stdout_handler.setFormatter(CustomFormatter())
+root_logger.addHandler(stdout_handler)
+
+
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-
-    # https://stackoverflow.com/a/44426266
-    logger.propagate = False
-
-    stdout_handler = logging.StreamHandler(stdout)
-    stdout_handler.setFormatter(CustomFormatter())
-    logger.addHandler(stdout_handler)
 
     return logger
