76e58d433b6a8e003b94a1325d5ef8ce5d096bd1
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-26 20:26:56 -0400

    ⚡ feat: implement the light sensor driver with an option to use randomized data (when working on the application not on the Raspberry Pi)

--- microcontroller_application/modules/m01_environment/software_components/sc04_light_sensor_driver.py
+++ microcontroller_application/modules/m01_environment/software_components/sc04_light_sensor_driver.py
@@ -5,11 +5,14 @@ Component: 04. Light sensor driver
 This gets the ambient light intensity (in lux) every second or so.
 """
 
+from asyncio import sleep
+
 import bounded_channel
+import numpy as np
+
 from microcontroller_application.interfaces.message_types import (
     FromEnvironmentToControl,
 )
-
 from microcontroller_application.log import get_logger
 
 LOGGER = get_logger(__name__)
@@ -18,10 +21,48 @@ LOGGER = get_logger(__name__)
 async def run(
     *,
     to_control: bounded_channel.Sender[FromEnvironmentToControl],
+    use_randomized_data: bool,
 ):
     LOGGER.debug("startup")
 
-    # TODO
-    LOGGER.error("this hasn't been programmed yet, so it doesn't do anything")
+    if use_randomized_data:
+        LOGGER.warning("using randomized data")
+
+        while True:
+            lux = 2 ** (np.random.normal(10, 2))
+
+            LOGGER.info("(RANDOM) lux: %f", lux)
+
+            # This is just an approximation
+            as_lumens = lux * (600 / 20000)
+
+            await to_control.send(
+                FromEnvironmentToControl(ambient_brightness=as_lumens)
+            )
+
+            await sleep(1)
+
+    else:
+        import board
+        import busio
+        import adafruit_tsl2591
+
+        i2c = busio.I2C(board.SCL, board.SDA)
+
+        light_sensor = adafruit_tsl2591.TSL2591(i2c)
+
+        while True:
+            lux = light_sensor.lux
+
+            LOGGER.info("lux: %f", lux)
+
+            # This is just an approximation
+            as_lumens = lux * (600 / 20000)
+
+            await to_control.send(
+                FromEnvironmentToControl(ambient_brightness=as_lumens)
+            )
+
+            await sleep(1)
 
     LOGGER.debug("shutdown")
