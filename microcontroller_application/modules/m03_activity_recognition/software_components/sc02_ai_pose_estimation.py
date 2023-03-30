1225724a82769897989856d9e566f7e24048ee18
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-29 20:28:40 -0400

    write AI pose estimation component

---
+++ microcontroller_application/modules/m03_activity_recognition/software_components/sc02_ai_pose_estimation.py
@@ -0,0 +1,39 @@
+"""
+Module: 03. Activity recognition
+Component: 02. AI pose estimation
+"""
+
+import bounded_channel
+
+from microcontroller_application.interfaces.message_types import (
+    Activity,
+    FromActivityRecognitionToControl,
+    FromHumanDetectionToActivityRecognition,
+)
+from microcontroller_application.log import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+async def run(
+    *,
+    from_human_detection: bounded_channel.Receiver[
+        FromHumanDetectionToActivityRecognition
+    ],
+    to_control: bounded_channel.Sender[FromActivityRecognitionToControl],
+):
+    "Run the AI pose estimation software component"
+
+    LOGGER.debug("startup")
+
+    async for message in from_human_detection:
+        humans = message.images_of_humans
+        LOGGER.warning("this hasn't been programmed yet, so it doesn't do anything")
+
+        activities_of_humans = [Activity.NEITHER for _human in humans]
+
+        await to_control.send(
+            FromActivityRecognitionToControl(activities_of_humans=activities_of_humans)
+        )
+
+    LOGGER.debug("shutdown")
