5e8b82fd5b10067ca1166d01ed2bfa4e59954767
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-29 20:28:54 -0400

    write activity recognition module

--- microcontroller_application/modules/m03_activity_recognition/__init__.py
+++ microcontroller_application/modules/m03_activity_recognition/__init__.py
@@ -1,9 +1,12 @@
 """
+Module: 03. Activity recognition
+
 This module is concerned with categorizing
 what each human in the room is doing.
 """
 
-from bounded_channel import Receiver, Sender
+from asyncio import gather
+import bounded_channel
 
 from microcontroller_application.interfaces.message_types import (
     FromActivityRecognitionToControl,
@@ -11,15 +14,27 @@ from microcontroller_application.interfaces.message_types import (
 )
 from microcontroller_application.log import get_logger
 
+from .software_components import sc02_ai_pose_estimation
+
 LOGGER = get_logger(__name__)
 
 
 async def run(
     *,
-    from_human_detection: Receiver[FromHumanDetectionToActivityRecognition],
-    to_control: Sender[FromActivityRecognitionToControl],
+    from_human_detection: bounded_channel.Receiver[
+        FromHumanDetectionToActivityRecognition
+    ],
+    to_control: bounded_channel.Sender[FromActivityRecognitionToControl],
 ):
     "Run the activity recognition module"
 
     LOGGER.debug("startup")
+
+    sc02_ai_pose_estimation_task = sc02_ai_pose_estimation.run(
+        from_human_detection=from_human_detection,
+        to_control=to_control,
+    )
+
+    await gather(sc02_ai_pose_estimation_task)
+
     LOGGER.debug("shutdown")
