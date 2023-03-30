f60300273a2f24d803d4fb03c39d6caa3d804547
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-29 20:28:03 -0400

    write camera driver

--- microcontroller_application/modules/m01_environment/software_components/sc02_camera_driver.py
+++ microcontroller_application/modules/m01_environment/software_components/sc02_camera_driver.py
@@ -8,13 +8,17 @@ and it'll be easy for the aggregation module to convert it to a PIL Image
 when it's recording the feed.
 """
 
+from asyncio import sleep
+
 import bounded_channel
+import numpy as np
 
 from microcontroller_application.interfaces.message_types import (
     FromEnvironmentToAggregation,
     FromEnvironmentToHumanDetectionCameraFrame,
 )
 from microcontroller_application.log import get_logger
+from utils.asynchronous import at_least_one
 
 LOGGER = get_logger(__name__)
 
@@ -29,7 +33,36 @@ async def run(
 ):
     LOGGER.debug("startup")
 
-    # TODO
-    LOGGER.error("this hasn't been programmed yet, so it doesn't do anything")
+    if use_randomized_data:
+        LOGGER.warning("using randomized data")
+        LOGGER.error("this hasn't been programmed yet, so it doesn't do anything")
+
+    else:
+        from picamera import PiCamera
+
+        camera = PiCamera()
+
+        width = 768
+        height = 576
+
+        camera.resolution = (width, height)
+        camera.framerate = 24
+
+        while True:
+            frame = np.empty((height, width, 3), dtype=np.uint8)
+            camera.capture(frame, "rgb")
+
+            LOGGER.info("captured camera frame %r", frame)
+
+            await at_least_one(
+                [
+                    to_human_detection.send(
+                        FromEnvironmentToHumanDetectionCameraFrame(frame=frame)
+                    ),
+                    to_aggregation.send(FromEnvironmentToAggregation(frame=frame)),
+                ]
+            )
+
+            await sleep(1 / 30)
 
     LOGGER.debug("shutdown")
